0a50d4c854fddfcdb29d88b062f731893d0d1cae
leojmid2 <leojmid@gmail.com>
2021-02-22 17:52:57 -0800

    Welcome page version

---
+++ src/com/lockedMe/LockedMeMain.java
@@ -0,0 +1,25 @@
+package com.lockedMe;
+
+
+
+
+public class LockedMeMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("***********************************");
+		System.out.println("***********************************");
+		System.out.println("**                               **");
+		System.out.println("**                               **");
+		System.out.println("**    Welcome to LockedMe.com    **");
+		System.out.println("**                               **");
+		System.out.println("**          Developed by:        **");
+		System.out.println("**          Joel Dimazana        **");
+		System.out.println("**                               **");
+		System.out.println("**                               **");
+		System.out.println("***********************************");
+		System.out.println("***********************************");
+
+	}
+
+}
